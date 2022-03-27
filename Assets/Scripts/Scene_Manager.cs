c2f668949093bd331441461be0bd2aa6003f599e
AipNooBest <47401054+AipNooBest@users.noreply.github.com>
2022-03-27 19:36:52 +0300

    WIP

--- Assets/Scripts/Scene_Manager.cs
+++ Assets/Scripts/Scene_Manager.cs
@@ -5,7 +5,30 @@ using UnityEngine.SceneManagement;
 
 public class Scene_Manager : MonoBehaviour
 {
+    private static bool firstRiddleSolved = false;
+    private static bool secondRiddleSolved = false;
+    
     public static void changeScene(int i) {
         SceneManager.LoadScene(i);
-    } 
+    }
+
+    public static bool GetFirstRiddleSolved()
+    {
+        return firstRiddleSolved;
+    }
+    
+    public static bool GetSecondRiddleSolved()
+    {
+        return secondRiddleSolved;
+    }
+    
+    public static void SetFirstRiddleSolved(bool solved)
+    {
+        firstRiddleSolved = solved;
+    }
+    
+    public static void SetSecondRiddleSolved(bool solved)
+    {
+        secondRiddleSolved = solved;
+    }
 }
--- Assets/Scripts/Switcher.cs
+++ Assets/Scripts/Switcher.cs
@@ -70,5 +70,7 @@ public class Switcher : MonoBehaviour
         yield return new WaitForSeconds(2);
         var key = GameObject.Find("Key");
         Destroy(key);
+        Scene_Manager.changeScene(0);
+        Scene_Manager.SetFirstRiddleSolved(true);
     }
 }
